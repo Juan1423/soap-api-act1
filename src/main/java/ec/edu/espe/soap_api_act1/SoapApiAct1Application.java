18698f940fb9e2d25b361e2ff72a6ccbda41461a
Juan Ocaña <juandy.099@gmail.com>
2025-05-29 12:49:10 -0500

    Comiezo ya con las dependencia listas

---
+++ src/main/java/ec/edu/espe/soap_api_act1/SoapApiAct1Application.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.soap_api_act1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapApiAct1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapApiAct1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espe/soap_api_act1/SoapApiAct1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.soap_api_act1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapApiAct1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
